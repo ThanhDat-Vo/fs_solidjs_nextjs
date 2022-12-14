dc315c941f6046a43389f2a650352eaec3f44aa7
thanh-vo-opun-ai <thanh.vo@opun.ai>
2022-12-13 19:10:07 -0800

    up

---
+++ nextjs-appx/pages/index.tsx
@@ -0,0 +1,12 @@
+import fs from "fs";
+import styles from "../styles/Home.module.css";
+
+export default function Home({ fileContent }: { fileContent: string }) {
+  return <div className={styles.container}>{fileContent}</div>;
+}
+export async function getServerSideProps() {
+  const fileContent = String(await fs.readFileSync("./pages/index.tsx"));
+  return {
+    props: { fileContent }, // will be passed to the page component as props
+  };
+}
