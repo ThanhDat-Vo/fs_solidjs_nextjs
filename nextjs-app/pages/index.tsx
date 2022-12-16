5f66fe5a6a3872323d65ee34fbd68dc493ad15e5
thanh-vo-opun-ai <thanh.vo@opun.ai>
2022-12-16 13:41:03 -0800

    yp

--- nextjs-app/pages/index.tsx
+++ nextjs-app/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home({ fileContent }: { fileContent: string }) {
   return <div className={styles.container}>{fileContent}</div>;
 }
 export async function getServerSideProps() {
-  const fileContent = String(await fs.readFileSync("./pages/index.tsx"));
+  const fileContent = String(await fs.readFileSync(__filename));
   return {
     props: { fileContent }, // will be passed to the page component as props
   };
